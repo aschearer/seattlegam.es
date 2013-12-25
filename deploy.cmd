0c945c9ab4b09d829f44d2a5b129f6a630472ba1
Alex Schearer <aschearer@gmail.com>
2013-12-25 17:55:27 -0500

    Adding deployment configuration for Azure websites

---
+++ deploy.cmd
@@ -0,0 +1,12 @@
+:Deployment
+echo Installing dependencies
+call npm install
+
+echo Building app
+call .\node_modules\.bin\grunt.cmd prod
+
+echo Installing kudusync
+call npm install kudusync -g --silent
+
+echo Syncing artifacts
+call node "%appdata%\npm\node_modules\kuduSync\bin\kuduSync" -v 50 -f out -t ..\wwwroot -n ..\manifest -p ..\manifest
